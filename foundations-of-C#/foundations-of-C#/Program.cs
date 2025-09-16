0f0b17bf7b8aa69b75b0983e6bfb83f2479afbec
mksigrest <mksigrest@gmail.com>
2025-09-16 08:34:22 -0400

    code commit
    This commit has all the required code for the project

--- foundations-of-C#/foundations-of-C#/Program.cs
+++ foundations-of-C#/foundations-of-C#/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Formats.Asn1;
+
+OperatorExamples();
+
+void OperatorExamples() {
+    int width = 3;
+    width++;
+
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+    string result = "This Area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+
+    while (area < 50)
+    {
+        height++;
+        area = width * height;
+    }
+
+    do
+    {
+        width--;
+        area = width * height;
+    } while (area > 25);
+}
